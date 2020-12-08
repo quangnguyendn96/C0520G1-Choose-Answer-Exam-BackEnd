acdee5748c67aa2e0bdcd459e85f19fcebfdd319
nhatle352840 <leenhat11a1nh@gmail.com>
2020-12-08 17:36:53 +0700

    nhat commit

--- choose-anwser-exam/src/main/java/sprint_2/repository/UserRepository.java
+++ choose-anwser-exam/src/main/java/sprint_2/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package sprint_2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sprint_2.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+//    @Query(value = "select result_exam.user,sum(result_exam.mark) as `point`,count(result_exam.user) as `time` from result_exam\n" +
+//            "\tgroup by result_exam.user;,",nativeQuery = true)
+
 }
--- choose-anwser-exam/src/main/java/sprint_2/service/RoleService.java
+++ choose-anwser-exam/src/main/java/sprint_2/service/RoleService.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface RoleService {
     List<Role> findAll();
+
+    Role finById(Long idRole);
 }
