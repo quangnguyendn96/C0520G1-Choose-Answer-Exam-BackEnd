9704cbca6d273c7b96eb01fba8a04cf1bd89a13e
tungtyu1995 <tusytung@gmail.com>
2020-12-10 10:40:28 +0700

    tung commit back-end 10/12

--- choose-anwser-exam/src/main/java/sprint_2/commonUtils/ResponseMessage.java
+++ choose-anwser-exam/src/main/java/sprint_2/commonUtils/ResponseMessage.java
@@ -1,5 +1,20 @@
 package sprint_2.commonUtils;
 
+/**
+ * ResponseMessage
+ *
+ * Version 1.0
+ *
+ * Date: 08-12-2020
+ *
+ * Copyright
+ *
+ * Modification Logs:
+ * DATE                 AUTHOR          DESCRIPTION
+ * -----------------------------------------------------------------------
+ * 08-12-2020         TungTS            Response Message
+ */
+
 public class ResponseMessage {
     private String message;
 
